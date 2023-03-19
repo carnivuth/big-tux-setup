17ec0e136256b0263a11fe93f8e7d53301ae97a6
carnivuth <matti200042@gmail.com>
2023-03-19 11:07:50 +0100

    added install script

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 cp arch_home/.bashrc ~/.bashrc
 cp arch_home/.profile ~/.profile
+ls .config |while read SOURCE ; do
+echo "coping $SOURCE"
+cp -r ".config/$SOURCE" "/home/matteo/.config/$DEST"
+done
