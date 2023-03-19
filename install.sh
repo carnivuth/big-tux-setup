b1147993459121684e262fb5615351b6cb98642a
carnivuth <matti200042@gmail.com>
2023-03-19 11:29:53 +0100

    added custom application file

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 cp arch_home/.bashrc /home/matteo/.bashrc
 cp arch_home/.profile /home/matteo/.profile
-ls .config |while read SOURCE ; do
-echo "coping $SOURCE"
-cp -r ".config/$SOURCE" "/home/matteo/.config/$DEST"
+ls .config |while read FILE ; do
+echo "coping $FILE"
+cp -r ".config/$FILE" "/home/matteo/.config/$FILE"
+done
+ls .local/share/applications |while read FILE ; do
+echo "coping $FILE"
+cp -r ".local/share/applications/$FILE" "/home/matteo/.local/share/applications/$FILE"
+done
+ls .local/share/icons |while read FILE ; do
+echo "coping $FILE"
+cp -r ".local/share/icons/$FILE" "/home/matteo/.local/share/icons/$FILE"
 done
