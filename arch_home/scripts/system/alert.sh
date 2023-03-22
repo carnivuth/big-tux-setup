94527e113d29cc8956752be827fd9a5775ea34cc
carnivuth <matti200042@gmail.com>
2023-03-22 09:53:32 +0100

    backup of mer 22 mar 2023, 09:53:32, CET

--- arch_home/scripts/system/alert.sh
+++ arch_home/scripts/system/alert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$@" >> /home/matteo/notifications
-[[ "$1" != "Spotify" ]] && pacat --volume=1 -p ~/scripts/system/notification.wav 
+[[ "$1" != "Spotify" ]] && pacat --volume=1 -p ~/scripts/system/notification.wav
