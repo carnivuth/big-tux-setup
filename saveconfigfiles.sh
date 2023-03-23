8985bc80c107632f47542e7301000794f475ffae
carnivuth <matti200042@gmail.com>
2023-03-23 10:21:50 +0100

    backup of gio 23 mar 2023, 10:21:50, CET

--- saveconfigfiles.sh
+++ saveconfigfiles.sh
@@ -4,11 +4,20 @@ echo "coping $SOURCE"
 cp -r "$SOURCE" "$DEST"
 done
 echo "update pkgs list"
-rm archinstall_files/pacman_pkg
-rm archinstall_files/other_packets
+#remove old files
+rm archinstall_files/pacman_pkg.json
+rm archinstall_files/other_packets.json
+#create pacman's pkgs file
+echo "\"packages\":[">>archinstall_files/tmp.json
 pacman -Qen | cut -d ' ' -f1 | while read PKG; do 
- echo "\"$PKG\",">>archinstall_files/pacman_pkg
-done
+ echo "\"$PKG\",">>archinstall_files/tmp.json
+done 
+sed '$ s/.$//' archinstall_files/tmp.json > archinstall_files/pacman_pkg.json 
+rm archinstall_files/tmp.json
+echo "]">>archinstall_files/pacman_pkg.json
+## launch cpp to resolve include statements
+cpp -P archinstall_files/user_configuration.template -o archinstall_files/user_configuration.json
+#create other pkgs file
 pacman -Qem | cut -d ' ' -f1 | while read PKG; do 
- echo "\"$PKG\",">>archinstall_files/other_packets
+ echo "\"$PKG\",">>archinstall_files/other_packets.json
 done
