a22bcaeb1fd6bd8b06f0926ca14d0d551d67e26a
carnivuth <matti200042@gmail.com>
2023-03-23 09:29:46 +0100

    backup of gio 23 mar 2023, 09:29:46, CET

--- saveconfigfiles.sh
+++ saveconfigfiles.sh
@@ -3,3 +3,12 @@ cat configfiles |while read SOURCE DEST ; do
 echo "coping $SOURCE"
 cp -r "$SOURCE" "$DEST"
 done
+echo "update pkgs list"
+rm archinstall_files/pacman_pkg
+rm archinstall_files/other_packets
+pacman -Qen | cut -d ' ' -f1 | while read PKG; do 
+ echo "\"$PKG\",">>archinstall_files/pacman_pkg
+done
+pacman -Qem | cut -d ' ' -f1 | while read PKG; do 
+ echo "\"$PKG\",">>archinstall_files/other_packets
+done
