b56305c3288dcca9863aa26a05eb105c4a97027e
carnivuth <matti200042@gmail.com>
2023-06-30 08:27:03 +0200

    removed unused files

--- arch_home/scripts/system/alert.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "$@" >> /home/matteo/notifications
-[[ "$1" != "Spotify" ]] && pacat --volume=1 -p ~/scripts/system/notification.wav
--- arch_home/scripts/system/suspend.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dm-tool lock && systemctl suspend
